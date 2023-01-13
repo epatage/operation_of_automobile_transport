a18681e247512c22386830cf0b36b85710568860
Максим Макаров <makapoff@yandex.ru>
2023-01-13 13:23:56 +0400

    добавлено приложение Users

--- oat/oat/settings.py
+++ oat/oat/settings.py
@@ -23,6 +23,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'core.apps.CoreConfig',
+    'users.apps.UsersConfig',
     'cars.apps.CarsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -120,3 +122,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'users:login'
+LOGIN_REDIRECT_URL = 'cars:cars_list'
+# LOGOUT_REDIRECT_URL = 'posts:index'  # нужно править !!!
--- oat/oat/urls.py
+++ oat/oat/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('users.urls')),
+    path('auth/', include('django.contrib.auth.urls')),
     path('cars/', include('cars.urls', namespace='cars')),
-    #path('', include('applications.urls')),  # будет вести на главную страницу заявок
+    #path('', include('applications.urls')),  # будет выводить главную страницу заявок
 ]
